07e6bd3d05e9980244c6e078aa4107a0578d39c1
mgroth0 <mgroth49@gmail.com>
2022-09-04 18:51:38 -0400

    autocommit

--- src/commonMain/kotlin/matt/log/log.kt
+++ src/commonMain/kotlin/matt/log/log.kt
@@ -17,6 +17,7 @@ interface Prints {
 
 interface Logger {
   fun printLog(s: String)
+  fun tab(s: Any) = printLog("\t$s")
   operator fun plusAssign(s: Any) = printLog(s.toString())
   var startTime: Long?
 }
@@ -24,13 +25,13 @@ interface Logger {
 
 fun debug(s: Any) {
   if (DEBUG) {
-    println(s.toString())
+	println(s.toString())
   }
 }
 
 fun profile(s: Any) {
   if (PROFILE) {
-    println(s.toString())
+	println(s.toString())
   }
 }
 
@@ -43,19 +44,19 @@ fun warnIfNot(b: Boolean, w: ()->String) = warnIf(!b, w)
 
 fun warn(vararg s: Any) {
   s.forEach {
-    warned += it
-    println("WARNING:${it.toString().uppercase()}")
+	warned += it
+	println("WARNING:${it.toString().uppercase()}")
   }
 }
 
 fun warnOnce(s: Any) {
   if (s in warned) return
   else {
-    warn(s)
-    warned += s
-    if (warned.size > 100) {
-      throw RuntimeException("too many warnings")
-    }
+	warn(s)
+	warned += s
+	if (warned.size > 100) {
+	  throw RuntimeException("too many warnings")
+	}
   }
 }
 
@@ -63,8 +64,6 @@ fun warnOnce(s: Any) {
 fun tab(a: Any?) {
   println("\t${a}")
 }
-
-
 
 
 //fun taball(itr: DoubleArray) {
@@ -89,28 +88,28 @@ fun tab(a: Any?) {
 fun taball(s: String, itr: Collection<*>?) {
   println("$s(len=${itr?.size}):")
   itr?.forEach {
-    println("\t${it}")
+	println("\t${it}")
   }
 }
 
 fun taball(s: String, itr: DoubleArray?) {
   println("$s(len=${itr?.size}):")
   itr?.forEach {
-    println("\t${it}")
+	println("\t${it}")
   }
 }
 
 fun taball(s: String, itr: Array<*>?) {
   println("$s(len=${itr?.size}):")
   itr?.forEach {
-    println("\t${it}")
+	println("\t${it}")
   }
 }
 
 fun taball(s: String, itr: Iterable<*>?) {
   println("$s:")
   itr?.forEach {
-    println("\t${it}")
+	println("\t${it}")
   }
 }
 
@@ -122,8 +121,8 @@ fun taball(s: String, itr: Map<*, *>?) {
 val todos = mutableSetOf<String>()
 fun todo(vararg s: String) {
   s.forEach {
-    todos += it
-    println("todo: $it")
+	todos += it
+	println("todo: $it")
   }
 }
 
@@ -133,24 +132,23 @@ fun todoOnce(s: String) {
 }
 
 
-
 @OptIn(ExperimentalContracts::class)
 inline fun <T> T.takeUnlessPrintln(msg: String, predicate: (T)->Boolean): T? {
   contract {
-    callsInPlace(predicate, EXACTLY_ONCE)
+	callsInPlace(predicate, EXACTLY_ONCE)
   }
   return if (!predicate(this)) this else run {
-    println(msg)
-    null
+	println(msg)
+	null
   }
 }
 
 fun <T> analyzeExceptions(op: ()->T): T {
   try {
-    return op()
+	return op()
   } catch (e: Throwable) {
-    println("${e::class.simpleName} message: ${e.message}")
-    e.printStackTrace()
-    throw e
+	println("${e::class.simpleName} message: ${e.message}")
+	e.printStackTrace()
+	throw e
   }
 }
