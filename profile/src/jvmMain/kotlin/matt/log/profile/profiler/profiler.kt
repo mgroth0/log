ce52842b41c806df175339704bb6d03c18bd48f3
mgroth0 <mgroth49@gmail.com>
2022-10-11 11:18:26 -0400

    autocommit

--- profile/src/jvmMain/kotlin/matt/log/profile/profiler/profiler.kt
+++ profile/src/jvmMain/kotlin/matt/log/profile/profiler/profiler.kt
@@ -4,7 +4,6 @@ import matt.log.profile.profiler.ProfileRecursionType.ALL
 import matt.log.profile.profiler.ProfileRecursionType.DEEPEST_ONLY
 import matt.log.profile.profiler.ProfileRecursionType.NOT_ALLOWED
 import matt.log.profile.profiler.ProfileRecursionType.TOP_ONLY
-import matt.log.profile.profiler.ProfiledBlock.Companion
 import matt.log.profile.stopwatch.Stopwatch
 import matt.log.profile.stopwatch.tic
 import matt.log.report
