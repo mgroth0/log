973561a4f42d6d23d312c5c24d0b08f78947fa0e
mgroth0 <mgroth49@gmail.com>
2022-10-27 11:54:40 -0400

    autocommit

--- profile/src/jvmMain/kotlin/matt/log/profile/mem/mem.kt
+++ profile/src/jvmMain/kotlin/matt/log/profile/mem/mem.kt
@@ -2,7 +2,10 @@ package matt.log.profile.mem
 
 import matt.lang.RUNTIME
 import matt.model.byte.ByteSize
+import matt.model.byte.megabytes
 import matt.model.errreport.Report
+import matt.time.dur.sleep
+import kotlin.time.Duration.Companion.seconds
 
 class MemReport: Report() {
   val total = ByteSize(RUNTIME.totalMemory())
@@ -15,5 +18,15 @@ class MemReport: Report() {
 	s += "heapmaxsize:${max}\n"
 	s += "heapFreesize:${free}"
 	s
+  }
+}
+
+private val THROTTLE_THRESHOLD = 100.megabytes
+private val THROTTLE_INTERVAL = 1.seconds
+fun throttle(label: String) {
+  val free = ByteSize(RUNTIME.freeMemory())
+  if (free < THROTTLE_THRESHOLD) {
+	println("throttling $label because there is < $THROTTLE_THRESHOLD free...")
+	sleep(THROTTLE_INTERVAL)
   }
 }
