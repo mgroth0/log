bbf8e00ec1942707f8b9d040941bea410e5c054b
mgroth0 <mgroth49@gmail.com>
2023-04-25 21:50:36 -0400

    changed src/commonMain/kotlin/matt/log/warn/warn.kt

--- src/commonMain/kotlin/matt/log/warn/warn.kt
+++ src/commonMain/kotlin/matt/log/warn/warn.kt
@@ -1,33 +1,46 @@
 package matt.log.warn
 
 val warned = mutableSetOf<Any>()
-fun warnIf(b: Boolean, w: ()->String) {
-  if (b) warn(w())
+fun warnIf(b: Boolean, w: () -> String) {
+    if (b) warn(w())
 }
 
-fun warnIfNot(b: Boolean, w: ()->String) = warnIf(!b, w)
+fun warnIfNot(b: Boolean, w: () -> String) = warnIf(!b, w)
 
 fun warn(vararg s: Any, upper: Boolean = true) {
-  s.forEach {
-	warned += it
-	println("WARNING:${it.toString().let { if (upper) it.uppercase() else it }}")
-  }
+    s.forEach {
+        warned += it
+        println("WARNING:${it.toString().let { if (upper) it.uppercase() else it }}")
+    }
 }
 
 expect fun dumpStack()
 
 fun warnAndDumpStack(vararg s: Any) {
-  warn(*s)
-  dumpStack()
+    warn(*s)
+    dumpStack()
 }
 
 fun warnOnce(s: Any) {
-  if (s in warned) return
-  else {
-	warn(s)
-	warned += s
-	if (warned.size > 100) {
-	  throw RuntimeException("too many warnings")
-	}
-  }
+    if (s in warned) return
+    else {
+        warn(s)
+        warned += s
+        if (warned.size > 100) {
+            throw RuntimeException("too many warnings")
+        }
+    }
+}
+
+
+val printlnOnceMemory = mutableSetOf<String>()
+fun printlnOnce(s: String) {
+    if (s in printlnOnceMemory) return
+    else {
+        println(s)
+        printlnOnceMemory += s
+        if (printlnOnceMemory.size > 100) {
+            throw RuntimeException("too many printlnOnces")
+        }
+    }
 }
